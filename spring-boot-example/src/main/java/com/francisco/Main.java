baf34cfa676b4828c0030a222ada616d0bea49a7
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-29 12:52:44 -0300

    URL

--- spring-boot-example/src/main/java/com/francisco/Main.java
+++ spring-boot-example/src/main/java/com/francisco/Main.java
@@ -3,6 +3,7 @@ package com.francisco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +17,9 @@ public class Main {
     }
 
     @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse("Hello", List.of("Java", "Golang", "Javascript"),
+    public GreetResponse greet(@RequestParam(value = "name", required = false) String name) {
+        String greetMessage = name == null || name.isBlank() ? "Hello" : "Hello " + name;
+        return new GreetResponse(greetMessage, List.of("Java", "Golang", "Javascript"),
                 new Person("Alex", 28, 30_000));
     }
 
