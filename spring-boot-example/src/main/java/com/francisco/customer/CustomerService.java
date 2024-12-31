8b74adcc8ae4c9ac807a512992502ab0d1171b84
Francisco Sousa <franciscoeds1987@gmail.com>
2024-12-31 12:49:35 -0300

    RestController Service Repository Component annotations

--- spring-boot-example/src/main/java/com/francisco/customer/CustomerService.java
+++ spring-boot-example/src/main/java/com/francisco/customer/CustomerService.java
@@ -1,7 +1,10 @@
 package com.francisco.customer;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class CustomerService {
 
     private final CustomerDao customerDao;
