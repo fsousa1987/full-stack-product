a3beaec754b43ee6dc16c207439c2dba3c0c5caf
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-01 18:53:55 -0300

    Custom Exception

--- spring-boot-example/src/main/java/com/francisco/Main.java
+++ spring-boot-example/src/main/java/com/francisco/Main.java
@@ -2,19 +2,12 @@ package com.francisco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
-
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
-
-        for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
-        }
+        SpringApplication.run(Main.class, args);
     }
 
 }
---
+++ spring-boot-example/src/main/java/com/francisco/exception/ResourceNotFound.java
@@ -0,0 +1,9 @@
+package com.francisco.exception;
+
+public class ResourceNotFound extends RuntimeException {
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+
+}
