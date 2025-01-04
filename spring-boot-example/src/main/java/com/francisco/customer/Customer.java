2504ca63a7c38b0331b64b20cc3266f320a9cf05
Francisco Sousa <franciscoeds1987@gmail.com>
2025-01-04 19:38:59 -0300

    Column Constraints

--- spring-boot-example/src/main/java/com/francisco/customer/Customer.java
+++ spring-boot-example/src/main/java/com/francisco/customer/Customer.java
@@ -12,10 +12,13 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Integer id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private Integer age;
 
     public Customer() {
